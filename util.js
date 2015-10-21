ae76eb97718711c9537106b6345b9d37b898417f
claudiopro <claudio.procida@gmail.com>
2015-10-21 12:10:46 +0200

    Simplify utils

--- util.js
+++ util.js
@@ -1,11 +1,17 @@
 'use strict';
 
-const I_TO_A = 'ABCDEFGHIJ'.split('');
+const I_TO_A = 'ABCDEFGHIJ';
 
+/**
+ * Converts a zero-based coord to a letter e.g. 0->A, 1->B, ... 10->J
+ */
 function itoa(n) {
-  return I_TO_A[n];
+  return I_TO_A.charAt(n);
 }
 
+/**
+ * Converts a letter to a zero-based coord e.g. A->0, B->1, ... J->10
+ */
 function atoi(a) {
   return I_TO_A.indexOf(a.toUpperCase());
 }
